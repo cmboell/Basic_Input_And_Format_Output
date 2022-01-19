6f4f05ad41a6e25e7c3aaae2887e22e614b76dd1
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-01-19 16:13:03 -0600

    added header note and refactored names of the folder and the .py file

---
+++ format_output/Basic_Input_And_Format_Output.py
@@ -0,0 +1,10 @@
+"""
+Program: Basic_Input_And_Format_Output.py
+Author: Colby Boell
+Last date modified: 01/19/2022
+
+The purpose of this program is to prompt user for their first name, last name, age
+and 3 different test score values out of 100. Then take the 3 scores and find the average to store in a variable,
+then print out the user first and last name, age, and average score (formatted 2 decimal places) as a
+formatted sentence.
+"""
